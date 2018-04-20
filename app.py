192b183549d0669d328f9cd6a88b68e0989f23d1
unknown <boneric.rosete@gmail.com>
2018-04-20 15:58:40 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# This is a tutorial from Docker for learning docker compose feature
+# https://docs.docker.com/compose/gettingstarted/#step-1-setup
+
+
+import time
+import redis
+from flask import Flask
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello Bon! This website have been visited {} times. This is powered by Django Flask and Redis.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
